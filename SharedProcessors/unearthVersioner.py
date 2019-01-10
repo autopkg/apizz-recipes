d69c2b433d3f3aed0645b3346070d60d586723f3
AP Orlebeke <aporlebeke@gmail.com>
2019-01-10 16:58:41 -0500

    Add unearthVersioner shared processor

---
+++ SharedProcessors/unearthVersioner.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import os.path
+import subprocess
+
+from autopkglib import Processor, ProcessorError
+
+__all__ = ["unearthVersioner"]
+
+class unearthVersioner(Processor):
+    description = ("Gets the unearth executable version. ")
+    input_variables = {
+        "source_path": {
+            "required": False,
+            "description": "Full path to unearth executable. Defaults to %pkgroot%/usr/local/bin/unearth/unearth. "
+    	}
+    }
+    output_variables = {
+    	"version": {
+    		"description": "The unearth executable version"
+    	}
+    }
+
+    __doc__ = description
+
+    def main(self):
+		if not self.env['source_path']:
+			self.env['source_path'] = os.path.join(self.env['pkgroot'], "/usr/local/bin/unearth/unearth")
+
+		self.output("unearth path: %s" % self.env['source_path'])
+
+		if os.path.exists(self.env['source_path']):
+			self.output("Found unearth executable at %s" % self.env['source_path'])
+			try:
+				cmd = subprocess.check_output(['/usr/bin/python', self.env['source_path'], '-v'])
+				self.env['version'] = cmd.replace('\n', '')
+				self.output("Version: %s" % self.env['version'])
+			except OSError as e:
+				raise ProcessorError("Can't find %s" % (self.env['source_path'], e.strerror))
+		else:
+			raise ProcessorError("Could not find unearth executable at %s" % (self.env['source_path'], e.strerror))
+	
+if __name__ == '__main__':
+    processor = unearthVersioner()
+    processor.execute_shell()
