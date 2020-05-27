f0c80905c2cfb28c836b58d8964a814367322438
AP Orlebeke <aporlebeke@gmail.com>
2020-05-27 17:35:36 -0400

    Convert unearthVersioner to a generic ExecutableFileVersioner shared processor (#20)
    * Generalize unearthVersioner > ExecutableFileVersioner
    
    * Simplify processor
    
    - Add check for found_filename at beginning
    - Remove indent for try statement
    - Substitute 'replace' function with 'rstrip' for removing the trailing new line from collected version numbers
    
    * Update variable format to adhere to other processors
    
    * Add README for SharedProcessors
    
    - Add info for ExecutableFileVersioner

---
+++ SharedProcessors/ExecutableFileVersioner.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+from __future__ import absolute_import
+
+import os.path
+import subprocess
+
+from autopkglib import Processor, ProcessorError
+
+__all__ = ["ExecutableFileVersioner"]
+
+class ExecutableFileVersioner(Processor):
+    description = ("Gets the version of an executable file collected by FileFinder. ")
+    input_variables = {
+        "found_filename": {
+            "required": True,
+            "description": (
+                "Full path to the executable file collected "
+                "by FileFinder. "
+            ),
+        },
+        "interpreter_path": {
+            "required": False,
+            "default": "/usr/bin/python",
+            "description": (
+                "Full path to the interpreter to call the executable "
+                "file, if needed. "
+            ),
+        },
+        "version_argument": {
+            "required": True,
+            "description": (
+                "Argument passed to executable file to collect "
+                "its version. "
+            ),
+        }
+    }
+    output_variables = {
+        "version": { "description": "The executable file version" }
+    }
+
+    __doc__ = description
+
+    def main(self):
+        if not os.path.exists(self.env['found_filename']):
+            raise ProcessorError("\nCould not find executable at %s" % (self.env['found_filename']))
+        else: 
+            self.output("Found executable at %s" % self.env['found_filename'])
+
+        try:
+            cmd = subprocess.check_output([ self.env['interpreter_path'], self.env['found_filename'], self.env['version_argument']])
+            # Get version and remove offending new line at the end
+            self.env['version'] = cmd.rstrip('\n')
+            self.output("Version: %s" % self.env['version'])
+        except OSError:
+            raise ProcessorError("Can't find %s" % (self.env['found_filename']))
+            
+
+if __name__ == '__main__':
+    processor = ExecutableFileVersioner()
+    processor.execute_shell()
